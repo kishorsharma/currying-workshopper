61d9f15fbadb9f4551ba15e61488225c030b474b
Brij Kishor <kishorsharma7@gmail.com>
2015-09-07 20:12:11 +0530

    Updated exercise.js

--- exercises/identity/exercise.js
+++ exercises/identity/exercise.js
@@ -11,6 +11,13 @@ exercise = filecheck(exercise)
 
 // add setup.
 exercise.addSetup(function (mode, callback) {
+   console.log ("this", this);
+
+   console.log ("this.solutionModule", this.solutionModule);
+   console.log ("this.submissionModule", this.submissionModule);
+   console.log ("this.submissionArgs", this.submissionArgs);
+   console.log ("this.solutionArgs", this.solutionArgs);
+
     this.solutionModule = require(getSolutionPath() + 'solution.js');
     this.submissionModule = require([process.cwd(), this.args[0]].join('/'));
 
@@ -24,7 +31,8 @@ console.log("exercise: ", exercise);
 exercise.addProcessor(function (mode, callback) {
     var pass = true;
     var random = Math.random();
-    var idx = this.solutionModule(random);
+  //  var idx = this.solutionModule(random);
+    var idx = this.submissionModule(random);
 
     if (idx !== random) {
         exercise.emit('fail', 'this.ready was not set to true.');
@@ -39,24 +47,26 @@ exercise.addProcessor(function (mode, callback) {
 // Print out the suggested solution when the student passes. This is copied from
 // workshopper-exercise/execute because the rest of execute is not relevant to
 // the way this is tested.
-exercise.getSolutionFiles = function (callback) {
-    var solutionDir = getSolutionPath();
-
-    fs.readdir(solutionDir, function (err, list) {
-        if (err) {
-            return callback(err);
-        }
-
-        list = list
-            .filter(function (f) { return (/\.js$/).test(f) })
-            .map(function (f) { return path.join(solutionDir, f)});
-
-        callback(null, list);
-    });
-};
+// exercise.getSolutionFiles = function (callback) {
+//     var solutionDir = getSolutionPath();
+//
+//     fs.readdir(solutionDir, function (err, list) {
+//         if (err) {
+//             return callback(err);
+//         }
+//
+//         list = list
+//             .filter(function (f) { return (/\.js$/).test(f) })
+//             .map(function (f) { return path.join(solutionDir, f)});
+//
+//         callback(null, list);
+//     });
+// };
 
 function getSolutionPath() {
     return path.join(exercise.dir, './solution/');
 }
+// compare stdout of solution and submission
+// exercise = comparestdout(exercise)
 
 module.exports = exercise
