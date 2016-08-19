cc8a58b93f2781fdbe3e9b9d562f88735042d446
kishorsharma <brijkishor.cse@gmail.com>
2016-08-19 05:55:20 +0000

    Removed log from call and apply solution. 2. Updated the version.

--- exercises/call_and_apply/solution/solution.js
+++ exercises/call_and_apply/solution/solution.js
@@ -1,10 +1,8 @@
 var callAndApply = {
   caller: function (object, method, nameArg, ageArg, tShirtSizeArg) {
-    console.log("=============================",object, nameArg, ageArg, tShirtSizeArg)
     method.call(object, nameArg, ageArg, tShirtSizeArg)
   },
   applier: function (object, method, argumentsArr) {
-    console.log("=============================",object, argumentsArr)
     return method.apply(object, argumentsArr);
   }
 };
