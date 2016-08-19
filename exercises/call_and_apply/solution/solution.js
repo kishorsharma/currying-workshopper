fe8ef9e1cfe583d74c091f196cf6f73b0518185b
JoshKloster <JoshKloster@users.noreply.github.com>
2016-08-18 22:40:31 -0700

    Update solution.js
    Removed random letters that are unnecessary and confusing.

--- exercises/call_and_apply/solution/solution.js
+++ exercises/call_and_apply/solution/solution.js
@@ -1,10 +1,10 @@
 var callAndApply = {
   caller: function (object, method, nameArg, ageArg, tShirtSizeArg) {
-    console.log("========dfsssdfsdf=====================",object, nameArg, ageArg, tShirtSizeArg)
+    console.log("=============================",object, nameArg, ageArg, tShirtSizeArg)
     method.call(object, nameArg, ageArg, tShirtSizeArg)
   },
   applier: function (object, method, argumentsArr) {
-    console.log("========dfssdddddddddddsdfsdf=====================",object, argumentsArr)
+    console.log("=============================",object, argumentsArr)
     return method.apply(object, argumentsArr);
   }
 };
