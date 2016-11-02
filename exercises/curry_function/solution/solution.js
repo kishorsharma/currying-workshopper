f6429fefca3d69edfae6381bea96498c0b6dc450
Adam Szaraniec <adam.szaraniec@xsolve.pl>
2016-11-02 22:35:13 +0100

    Increase readability

--- exercises/curry_function/solution/solution.js
+++ exercises/curry_function/solution/solution.js
@@ -1,16 +1,14 @@
-function curry(fx) {
-  var arity = fx.length;
-  return function f1() {
-    var args = Array.prototype.slice.call(arguments, 0);
-    if (args.length >= arity) {
-      return fx.apply(null, args);
-    } else {
-      return function f2() {
-        var args2 = Array.prototype.slice.call(arguments, 0);
-        return f1.apply(null, args.concat(args2));
-      };
-    }
-  };
+function curry(fn) {
+	var params = [];
+
+	return function inner () {
+	  var args = Array.prototype.slice.call(arguments);
+	  Array.prototype.push.apply(params, args);
+
+	  return (params.length != fn.length)
+	    ? inner
+	    : fn.apply(this, params);
+	}
 }
 
 module.exports = curry;
